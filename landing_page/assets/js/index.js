3587c61cc14c9fad327b3669550c5110490620ed
Bikyoung Shon <103485405+Bikyoung@users.noreply.github.com>
2025-09-17 19:59:21 +0900

    recommend-sec 구현 완료
    슬라이드 기능 구현 완료 및 반응형 recommend-sec 구현 완료

--- landing_page/assets/js/index.js
+++ landing_page/assets/js/index.js
@@ -48,13 +48,18 @@ radioBtnArr.forEach((radioBtn) => {
     });
 });
 
+
+
+
 // recommend-sec에 슬라이드 구현
 // new Swiper(슬라이드가 적용될 요소, 옵션 객체)
 const recommendSecSwiper = new Swiper(".recommend-sec .swiper", {
-    // 한번에 보여줄 슬라이드 갯수를 지정
-    slidesPerView: 3,
-    // 슬라이드 간의 간격 (단위는 별도로 지정하지 않지만 픽셀임)
-    spaceBetween: 20,
+    /* 한번에 보여줄 슬라이드 갯수를 지정
+       단, 이로 인해 슬라이드의 너비가 swiper에 의해 자동 조절될 수 있음
+       자동 조절이 싫으면 속성 값을 "auto"로 정하고, 슬라이드의 너비를 직접 지정 
+    */
+    slidesPerView: "auto",
+
     // 마지막 슬라이드에서 다시 첫 슬라이드로 넘어가게 안함 (1-2-3-1 X)
     loop: false,
     // navigation으로 좌우 화살표 버튼 영역을 지정하면 자동으로 화살표 버튼이 생성됨
@@ -66,5 +71,21 @@ const recommendSecSwiper = new Swiper(".recommend-sec .swiper", {
     pagination: {
         el: ".swiper-indicator",
         clickable: true
+    },
+
+    // Swiper의 breakpoint
+    breakpoints: {
+        // 화면 너비가 1025px 이상일 때
+        1025: {
+            // 슬라이드 간의 간격 (단위는 별도로 지정하지 않지만 픽셀임)
+            spaceBetween: 26,
+        },
+        769: {
+            spaceBetween: 43
+
+        },
+        320: {
+            spaceBetween: 40
+        }
     }
 });
